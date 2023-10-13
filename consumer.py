cfc18b31b87b009da1bd6fd1a5b0cde81da87f7d
hussenmi <hussenmohammedak@gmail.com>
2023-10-13 15:02:54 -0400

    Added offset messagesto print

--- consumer.py
+++ consumer.py
@@ -3,4 +3,6 @@ from kafka import KafkaConsumer
 consumer = KafkaConsumer('my_topic', bootstrap_servers='localhost:9092', auto_offset_reset='earliest')
 
 for message in consumer:
-    print(f"Message: {message.value.decode('utf-8')}, Partition: {message.partition}")
+    print(f"Message: {message.value.decode('utf-8')}, Partition: {message.partition}, Offset: {message.offset}")
+
+consumer.close()
